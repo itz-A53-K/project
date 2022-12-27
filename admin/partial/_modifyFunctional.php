bcf3dc4fea0c82f918a4e9a8acaf21319b34bd3e
sk mamud ikbal <skmamudikbal@gmail.com>
2022-12-27 21:21:33 +0530

    go

--- admin/partial/_modifyFunctional.php
+++ admin/partial/_modifyFunctional.php
@@ -1,11 +1,12 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
+    include ('_dbConnect.php');
+
     $dist_id=$_POST['dist_id'];
     $vac_slot=$_POST['vac_slot'];
     $vac_stock=$_POST['vac_stock'];
 
     echo $dist_id.'/n'.$vac_slot.'/n'.$vac_stock.'/n';
-    include('_dbConnact.php')
     $sql="UPDATE `vaccine_dist_wise` SET  `slot` = '$vac_slot', `stock` = '$vac_stock' WHERE `vaccine_dist_wise`.`dist_id` = $dist_id";
     $result=mysqli_query($conn,$sql);
     if($result){

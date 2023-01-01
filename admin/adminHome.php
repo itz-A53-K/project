ad1222db70b8fafebc0842267c500fb5b0a6606d
sk mamud ikbal <skmamudikbal@gmail.com>
2023-01-01 19:54:43 +0530

    gh

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -27,13 +27,12 @@
         ';
         include 'partial/_alert_2.php';
 
-<<<<<<< HEAD
-        if ($_SERVER['REQUEST_METHOD']) {
+        if ($_SERVER['REQUEST_METHOD']=="POST") {
             include 'partial/_dbConnect.php';
         
             $id_num=$_POST['id_num'];
             $checkUser="SELECT * FROM `Book_slot` WHERE id_num='$id_num'";
-=======
+
         if ($_SERVER['REQUEST_METHOD']=="POST") {
            
             include 'partial/_dbConnect.php';
@@ -43,12 +42,12 @@
             $loginConfPass=$_POST['loginConfPass'];
 
             $checkUser="SELECT * FROM `users` WHERE userEmail='$loginEmail'";
->>>>>>> 5115bb1a321400c96d9f3b49e38778eacb47d902
+
             $result=mysqli_query($conn,$checkUser);
             $noOfRows=mysqli_num_rows($result);
         
             if ($noOfRows==1) {
-                echo69;
+                echo'69';
 
             }
         }
@@ -56,7 +55,7 @@
             
         echo '
         <form method="post" action="adminHome.php">
-<<<<<<< HEAD
+
            <div class="container">
             <center> <div class="select-box">
               <select>
@@ -66,7 +65,7 @@
                   <option>Voter ID</option>
              </select>
           </div></center>
-=======
+
         <div class="container">
        <center> <div class="select-box">
         <select>
@@ -76,7 +75,7 @@
             <option>Voter ID</option>
         </select>
     </div></center>
->>>>>>> 5115bb1a321400c96d9f3b49e38778eacb47d902
+
     <input type="text" id="id_num" name="id_num" placeholder="Enter ID Proof number"  >
         <input type="submit" value="Verify">
          

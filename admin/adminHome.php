c9e591f895c5751c72a8f1cad87f67293fe16d2c
sk mamud ikbal <skmamudikbal@gmail.com>
2023-01-01 20:08:51 +0530

    jk

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -31,18 +31,7 @@
             include 'partial/_dbConnect.php';
         
             $id_num=$_POST['id_num'];
-            $checkUser="SELECT * FROM `Book_slot` WHERE id_num='$id_num'";
-
-        if ($_SERVER['REQUEST_METHOD']=="POST") {
-           
-            include 'partial/_dbConnect.php';
-        
-            $loginEmail=$_POST['loginEmail'];
-            $loginPass=$_POST['loginPass'];
-            $loginConfPass=$_POST['loginConfPass'];
-
-            $checkUser="SELECT * FROM `users` WHERE userEmail='$loginEmail'";
-
+            $checkUser="SELECT * FROM `book_slot` WHERE id_num='$id_num'";
             $result=mysqli_query($conn,$checkUser);
             $noOfRows=mysqli_num_rows($result);
         
@@ -65,16 +54,6 @@
                   <option>Voter ID</option>
              </select>
           </div></center>
-
-        <div class="container">
-       <center> <div class="select-box">
-        <select>
-            <option hidden>ID Proof</option>
-            <option>Aadhar</option>
-            <option>Pan Card</option>
-            <option>Voter ID</option>
-        </select>
-    </div></center>
 
     <input type="text" id="id_num" name="id_num" placeholder="Enter ID Proof number"  >
         <input type="submit" value="Verify">
